2176efb3054ef6e811b115dda03b4b139a0d6e85
smartshopia <smartshopiapk@gmail.com>
2025-06-05 04:09:31 +0500

    sec change

--- fur_pdf_app/fur_pdf_app/settings.py
+++ fur_pdf_app/fur_pdf_app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'fur_pdf_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbmain.sqlite3',
     }
 }
 
